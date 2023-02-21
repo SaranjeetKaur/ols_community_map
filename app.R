9fad327d2ceb8e0ec82aa6c42ba5ab0dfbe47028
Bérénice Batut <bebatut@gmail.com>
2023-02-21 11:32:02 +0100

    Use data directly created in paper GitHub repo, remove local ones and scripts

--- app.R
+++ app.R
@@ -3,32 +3,36 @@ library(leaflet)
 library(dplyr)
 library(shiny)
 
-df <- read.csv("data/olscommunity.csv")
+df <- read.csv('https://raw.githubusercontent.com/open-life-science/ols-program-paper/main/data/people_per_cohort.csv')
 
 ui <- fillPage(
   tags$style(type = "text/css", "html, body {width:100%; height:100%}"),
   titlePanel("OLS Community Map"),
   leafletOutput("mymap", width = "100%", height = "100%"),
-  absolutePanel(top = 10, right = 10,
-                selectInput(inputId = "ols_cohort",
-                            label = "Choose the cohort you want to see:",
-                            choices = list("OLS 1" = "OLS_1", 
-                                           "OLS 2" = "OLS_2",
-                                           "OLS 3" = "OLS_3",
-                                           "OLS 4" = "OLS_4",
-                                           "OLS 5" = "OLS_5",
-                                           "OLS 6" = "OLS_6"))
+  absolutePanel(
+    top = 10,
+    right = 10,
+    selectInput(
+      inputId = "cohort",
+      label = "Choose the cohort you want to see:",
+      choices = list(
+        "OLS 1" = "1",
+        "OLS 2" = "2",
+        "OLS 3" = "3",
+        "OLS 4" = "4",
+        "OLS 5" = "5",
+        "OLS 6" = "6"))
   )
 )
 
 server <- function(input, output, session) {
   output$mymap <- renderLeaflet({
-    leaflet(df %>%
-              dplyr::filter(
-                ols_cohort == input$ols_cohort
-              )) %>%
-      addTiles() %>%
-      addMarkers(lat = ~latitude, lng=~longitude)
+    leaflet(
+      df %>%
+      dplyr::filter(cohort == input$cohort)
+    ) %>%
+    addTiles() %>%
+    addMarkers(lat = ~latitude, lng=~longitude)
   })
 }
 

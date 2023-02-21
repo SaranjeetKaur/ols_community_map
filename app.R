18199f031f4186b353475279df845319149da8e0
Bérénice Batut <bebatut@gmail.com>
2023-02-21 11:52:09 +0100

    Add different colors for the different roles

--- app.R
+++ app.R
@@ -5,6 +5,53 @@ library(shiny)
 
 df <- read.csv('https://raw.githubusercontent.com/open-life-science/ols-program-paper/main/data/people_per_cohort.csv')
 
+leafIcons <- iconList(
+  participant = makeIcon(
+    iconUrl = "https://leafletjs.com/examples/custom-icons/leaf-green.png",
+    iconWidth = 38,
+    iconHeight = 95,
+    iconAnchorX = 22,
+    iconAnchorY = 94,
+    shadowUrl = "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    shadowWidth = 50,
+    shadowHeight = 64,
+    shadowAnchorX = 4,
+    shadowAnchorY = 62),
+  mentor = makeIcon(
+    iconUrl = "https://leafletjs.com/examples/custom-icons/leaf-red.png",
+    iconWidth = 38,
+    iconHeight = 95,
+    iconAnchorX = 22,
+    iconAnchorY = 94,
+    shadowUrl = "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    shadowWidth = 50,
+    shadowHeight = 64,
+    shadowAnchorX = 4,
+    shadowAnchorY = 62),
+  expert = makeIcon(
+    iconUrl = "https://leafletjs.com/examples/custom-icons/leaf-orange.png",
+    iconWidth = 38,
+    iconHeight = 95,
+    iconAnchorX = 22,
+    iconAnchorY = 94,
+    shadowUrl = "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    shadowWidth = 50,
+    shadowHeight = 64,
+    shadowAnchorX = 4,
+    shadowAnchorY = 62),
+  speaker = makeIcon(
+    iconUrl = "https://leafletjs.com/examples/custom-icons/leaf-orange.png",
+    iconWidth = 38,
+    iconHeight = 95,
+    iconAnchorX = 22,
+    iconAnchorY = 94,
+    shadowUrl = "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    shadowWidth = 50,
+    shadowHeight = 64,
+    shadowAnchorX = 4,
+    shadowAnchorY = 62)
+)
+
 ui <- fillPage(
   tags$style(type = "text/css", "html, body {width:100%; height:100%}"),
   titlePanel("OLS Community Map"),
@@ -27,12 +74,9 @@ ui <- fillPage(
 
 server <- function(input, output, session) {
   output$mymap <- renderLeaflet({
-    leaflet(
-      df %>%
-      dplyr::filter(cohort == input$cohort)
-    ) %>%
+    leaflet(data = df %>% dplyr::filter(cohort == input$cohort)) %>%
     addTiles() %>%
-    addMarkers(lat = ~latitude, lng=~longitude)
+    addMarkers(lat = ~latitude, lng = ~longitude, icon = ~leafIcons[role])
   })
 }
 
